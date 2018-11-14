91e47ccb92c893fd8b7c910a628d6340e7138e96
Pavel Zhdanovich <swatoplus@gmail.com>
2018-11-14 18:15:02 +0300

    Added AesOfb

--- CryptoDocs.Services/ICryptoProviderResolver.cs
+++ CryptoDocs.Services/ICryptoProviderResolver.cs
@@ -1,30 +1,9 @@
-﻿using System;
-using CryptoDocs.Abstractions;
-using CryptoDocs.Shared.Symmetric;
+﻿using CryptoDocs.Shared.Symmetric;
 
 namespace CryptoDocs.Services
 {
     public interface ICryptoProviderResolver
     {
         IDataCryptoProvider GetCryptoProvider(string algorithm);
-    }
-
-    public class CryptoProviderResolver : ICryptoProviderResolver
-    {
-        private readonly IDataCryptoProvider _ideaCbc = new CbcCryptoProvider(new IdeaCryptoProvider());
-        private readonly IDataCryptoProvider _serpentCbc = new CbcCryptoProvider(new SerpentCryptoProvider());
-
-        public IDataCryptoProvider GetCryptoProvider(string algorithm)
-        {
-            switch (algorithm)
-            {
-                case CryptoAlgorithm.IdeaCbc:
-                    return _ideaCbc;
-                case CryptoAlgorithm.SerpentCbc:
-                    return _serpentCbc;
-                default:
-                    throw new InvalidOperationException();
-            }
-        }
     }
 }
